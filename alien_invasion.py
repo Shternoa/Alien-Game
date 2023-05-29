318b4c4c7a3097802e5a703b234e046d90c7ce7e
Elman <134933773+Shternoa@users.noreply.github.com>
2023-05-29 15:44:40 +0300

    Вывод корабля

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -11,6 +12,7 @@ def run_game():
     al_inv = Settings()
     screen = pygame.display.set_mode((al_inv.screen_width, al_inv.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    ship = Ship(screen)
 
     # Запуск основного цикла игры
     while True:
@@ -20,6 +22,8 @@ def run_game():
                 sys.exit()
         # Перерисовываем экран в другой цвет
         screen.fill(al_inv.bg_color)
+        # Показывает корабль
+        ship.shipdraw()
         # Показывает последний прорисованный экран
         pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -7,12 +7,14 @@ class Ship():
         self.screen = screen
 
         #
-        self.image = pygame.image.load('b0c34caf4d3814d69143af5bba2ce6d8.bmp')
+        self.image = pygame.image.load(r'C:\Users\evilp\PycharmProjects\Study2\Alien Game\images\ship.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
         #
-        self.rect.centerx = self.screen_center.centrex
-        self.rect.bottom = self.screen_bottom.bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
     def shipdraw(self):
         """Рисует корабль"""
-        self.screen.draw(self.image, self.rect)
+        self.screen.blit(self.image, self.rect)
+
