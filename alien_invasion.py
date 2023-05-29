b871eb0ff6dfba8afe1370633b1c9691be3db84a
Elman <134933773+Shternoa@users.noreply.github.com>
2023-05-29 14:30:45 +0300

    Инициализация игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,22 @@
+import sys
+
 import pygame
+
+
+def run_game():
+    '''Инициализация игры c объектами на экране'''
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 900))
+    pygame.display.set_caption('Alien Invasion')
+
+    # Запуск основного цикла игры
+    while True:
+        # Отслеживание клавиатуры и мышки
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Показывает последний прорисованный экран
+        pygame.display.flip()
+
+
+run_game()
