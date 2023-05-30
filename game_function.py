6796988bb50e317966d4b2d224a41de4013046d5
Elman <134933773+Shternoa@users.noreply.github.com>
2023-05-30 14:43:01 +0300

    Рефакторинг

--- game_function.py
+++ game_function.py
@@ -3,21 +3,31 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Реагированиеи нажатия клавиш"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Реакция на прекращения нажатия"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     # Отслеживание клавиатуры и мышки
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(al_inv_settings, screen, ship):
@@ -27,4 +37,3 @@ def update_screen(al_inv_settings, screen, ship):
     ship.shipdraw()
     # Показывает последний прорисованный экран
     pygame.display.flip()
-
